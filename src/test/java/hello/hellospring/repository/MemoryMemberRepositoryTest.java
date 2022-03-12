b7db124a0403e55ad0a01d206e6539435db67b6b
ratellll <latellbin@naver.com>
2022-03-12 09:55:27 +0900

    회원서비스 개발

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -4,53 +4,63 @@ import hello.hellospring.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
 import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.*;
 
-class MemoryMemberRepositoryTest {
+class MemoryMemberRepositoryTest{
+
     MemoryMemberRepository repository = new MemoryMemberRepository();
+
     @AfterEach
     public void afterEach() {
         repository.clearStore();
     }
+
     @Test
-    public void save() {
-//given
+    public void save(){
         Member member = new Member();
         member.setName("spring");
-//when
+
         repository.save(member);
-//then
+
         Member result = repository.findById(member.getId()).get();
-        assertThat(result).isEqualTo(member);
+        assertThat(member).isEqualTo(result);
+
     }
+
     @Test
-    public void findByName() {
-        //given
+    public void findByName(){
         Member member1 = new Member();
         member1.setName("spring1");
         repository.save(member1);
+
         Member member2 = new Member();
         member2.setName("spring2");
         repository.save(member2);
-        //when
+
         Member result = repository.findByName("spring1").get();
-        //then
+
         assertThat(result).isEqualTo(member1);
     }
+
     @Test
     public void findAll() {
-//given
         Member member1 = new Member();
         member1.setName("spring1");
         repository.save(member1);
+
         Member member2 = new Member();
         member2.setName("spring2");
         repository.save(member2);
-//when
+
         List<Member> result = repository.findAll();
-//then
+
         assertThat(result.size()).isEqualTo(2);
+
+
+
     }
 }
