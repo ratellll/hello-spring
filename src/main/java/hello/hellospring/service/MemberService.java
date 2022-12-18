c51faa635a1d58b67f7580dd1fbed7e6dd493bbc
ratellll <latellbin@naver.com>
2022-12-18 23:38:04 +0900

    컴포넌트 스캔 , 자동 의존관계 설정

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -4,12 +4,14 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Component
 public class MemberService {
 
     private final MemberRepository memberRepository;
