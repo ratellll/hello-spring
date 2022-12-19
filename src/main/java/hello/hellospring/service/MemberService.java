b497869dcb485419210239be72755b6a14c64920
ratellll <latellbin@naver.com>
2022-12-19 23:20:28 +0900

    mac h2 설치 및 JPA확인
    Entitymanager체크

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,6 +25,8 @@ public class MemberService {
      * 회원가입
      *
      */
+
+    @Transactional
     public Long join(Member member) {
         //같은이름이 있는 중복 회원x
         validateDuplicateMember(member);//중복회원 검증
