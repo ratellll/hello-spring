c3bc3916c93db68524514433dc7b5769d5a4a832
ratellll <latellbin@naver.com>
2024-02-06 23:36:43 +0900

    feat: 스프링 연습하기 데이터넘길때 url에서 &가 and역할을한다

--- src/main/java/hello/hellospring/Controller/HelloController.java
+++ src/main/java/hello/hellospring/Controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -13,5 +14,15 @@ public class HelloController {
         model.addAttribute("data", "제대로 해부리자~!");
         model.addAttribute("data1", "엉딩덩");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name,
+                           @RequestParam("age") String age,
+                            @RequestParam("score") String score ,Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+        model.addAttribute("score", score);
+        return "hello-template";
     }
 }
