189591e74b029e44ff5c3a533811d4c7049bda92
ratellll <latellbin@naver.com>
2024-02-07 23:03:24 +0900

    feat: 스프링 연습하기 mvc설계

--- src/main/java/hello/hellospring/Controller/HelloController.java
+++ src/main/java/hello/hellospring/Controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,10 +20,39 @@ public class HelloController {
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name,
                            @RequestParam("age") String age,
-                            @RequestParam("score") String score ,Model model) {
+                           @RequestParam("score") String score, Model model) {
         model.addAttribute("name", name);
         model.addAttribute("age", age);
         model.addAttribute("score", score);
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello"+name;
+
+    }
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.name = "빈이";
+        return hello;
+
+    }
+
+    static class Hello{
+
+        String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
