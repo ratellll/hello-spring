9e0eeae5bc11eca74426674f2bdc44a4bb8982e2
ratellll <latellbin@naver.com>
2024-06-18 16:12:14 +0900

    feat: 스프링 연습하기 mvc설계

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,44 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     * @param member
+     * @return
+     */
+    public Long join(Member member) {
+        //같은이름 중복회원 가입 x
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m ->{
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+                        });
+    }
+
+
+    public List<Member> findMember() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
