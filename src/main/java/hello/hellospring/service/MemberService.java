32861bb4c8d2762b77bd4747719d882a73a5365a
ratellll <latellbin@naver.com>
2024-06-21 18:17:22 +0900

    feat: 스프링 연습하기 mvc설계

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -10,7 +10,11 @@ import java.util.Optional;
 public class MemberService {
 
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원가입
@@ -30,7 +34,7 @@ public class MemberService {
         memberRepository.findByName(member.getName())
                         .ifPresent(m ->{
             throw new IllegalStateException("이미 존재하는 회원입니다");
-                        });
+                        }); // Optional을 객체에 담지않고 바로 반환한다
     }
 
 
